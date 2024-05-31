b41ddca398a4c72e182b5b11b3eebe92e7169f7e
ITSTIME <88642524+ITSTIME1@users.noreply.github.com>
2024-05-31 22:03:09 +0900

    build : spring boot was initialized

---
+++ logic/src/main/java/com/spring_greens/logic/LogicApplication.java
@@ -0,0 +1,13 @@
+package com.spring_greens.logic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicApplication.class, args);
+	}
+
+}
